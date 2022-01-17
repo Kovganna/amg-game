06bd49f4460bcc813c6ae6dfd3d40ef602a20a6e
Ann <annakovalenko168@gmail.com>
2022-01-17 14:13:06 +0200

    done

---
+++ app.js
@@ -0,0 +1,115 @@
+const startBtn = document.querySelector("#start");
+
+const screens = document.querySelectorAll(".screen");
+
+const timeList = document.querySelector("#time-list");
+
+const timeEl = document.querySelector("#time");
+
+const board = document.querySelector("#board");
+
+// const colors = [
+//   "#94ff7f",
+//   "#f07fff",
+//   "#e74c3c",
+//   "#7f92ff",
+//   "#3498db",
+//   "#e67e22",
+//   "#8e44ad",
+//   "#2ecc71",
+// ];
+
+let time = 0;
+let score = 0;
+
+startBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  screens[0].classList.add("up");
+});
+
+timeList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("time-btn")) {
+    time = parseInt(event.target.getAttribute("data-time"));
+    screens[1].classList.add("up");
+    startGame();
+  }
+});
+
+board.addEventListener("click", (event) => {
+  if (event.target.classList.contains("circle")) {
+    score++;
+    event.target.remove();
+    createRandomCircle();
+  }
+});
+
+function startGame() {
+  setInterval(decreaseTime, 1000);
+  createRandomCircle();
+  setTime(time);
+}
+
+function decreaseTime() {
+  if (time === 0) {
+    finish();
+  } else {
+    let currentTime = --time;
+    if (currentTime < 10) {
+      currentTime = `0${currentTime}`;
+    }
+    setTime(currentTime);
+  }
+}
+
+function setTime(value) {
+  timeEl.innerHTML = `00:${value}`;
+}
+
+function finish() {
+  //   timeEl.parentNode.remove();
+  timeEl.parentNode.classList.add("hide");
+
+  board.innerHTML = `<h1>Ваш счет: <span class="primary">${score}</span></h1>`;
+}
+
+function createRandomCircle() {
+  const circle = document.createElement("div");
+
+  const size = getRandomNumber(10, 50);
+  const { width, height } = board.getBoundingClientRect();
+
+  const x = getRandomNumber(0, width - size);
+  const y = getRandomNumber(0, height - size);
+
+  circle.classList.add("circle");
+  circle.style.width = `${size}px`;
+  circle.style.height = `${size}px`;
+  circle.style.top = `${x}px`;
+  circle.style.left = `${y}px`;
+
+  setColor(circle);
+  board.append(circle);
+}
+
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function setColor(elem) {
+  const color = getRandomColor();
+
+  elem.style.backgroundColor = color;
+  elem.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function getRandomColor() {
+  //   const index = Math.floor(Math.random() * colors.length);
+  //   return colors[index];
+  const color = Math.floor(Math.random() * Math.pow(256, 3)).toString(16);
+
+  while (color.length < 6) {
+    color = "0" + color;
+  }
+
+  return "#" + color;
+}
